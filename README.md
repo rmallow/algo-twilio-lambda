0bedf1be11ced57dad9d178c829fe15f1527123e
rmallow <rmallow@crimson.ua.edu>
2020-12-19 19:18:16 -0600

    updating readme for what's actually going on here

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # algo-twilio-lambda
+
+The code on my lambda function for using twilio to interact with algo.
--- __version__.py
+++ __version__.py
@@ -1,3 +1,3 @@
 # pylint: skip-file
-VERSION = (0, 0, 1)
+VERSION = (0, 0, 2)
 __version__ = '.'.join(map(str, VERSION))
